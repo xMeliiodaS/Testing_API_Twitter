3603796e238e60121cdeb60d2fe871b64ef0d56f
xMeliiodaS <bahaa.abozlf57@gmail.com>
2024-07-23 09:40:09 +0300

    Changed the Header in the config file since my free plan ended

--- logic/api/utils.py
+++ logic/api/utils.py
@@ -12,27 +12,35 @@ class Utils:
         Returns:
             dict: The tweet with the matching ID, or None if not found.
         """
-        for tweet in tweets:
-            if tweet["tweet_id"] == tweet_id:
-                return tweet
-        return None
+        filtered_tweets = list(filter(lambda tweet: tweet["tweet_id"] == tweet_id, tweets))
+        return filtered_tweets[0]
 
     @staticmethod
     def find_follower_by_user_id(followers, user_id):
         """
         Helper function to find a follower by user_id.
+
+        Args:
+            followers (list): The list of followers to search through.
+            user_id (str): The user_id of the follower to find.
+
+        Returns:
+            dict: The follower with the matching user_id, or None if not found.
         """
-        for follower in followers:
-            if follower["user_id"] == user_id:
-                return follower
-        return None
+        filtered_followers = list(filter(lambda follower: follower["user_id"] == user_id, followers))
+        return filtered_followers[0]
 
     @staticmethod
     def find_user_media_by_user_id(media_list, user_id):
         """
         Finds and returns a user media item from the list by user_id.
+
+        Args:
+            media_list (list): The list of media items to search through.
+            user_id (str): The user_id of the media item to find.
+
+        Returns:
+            dict: The media item with the matching user_id, or None if not found.
         """
-        for media in media_list:
-            if media["user"]["user_id"] == user_id:
-                return media
-        return None
+        filtered_media = list(filter(lambda media: media["user"]["user_id"] == user_id, media_list))
+        return filtered_media[0]
